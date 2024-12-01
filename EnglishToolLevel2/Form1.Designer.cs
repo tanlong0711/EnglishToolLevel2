76f6fcb0d7dfd606fdb612571440228e0adbdace
Tan Long <longht0711@gmail.com>
2024-12-01 21:10:32 +0700

    new project

---
+++ EnglishToolLevel2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace EnglishToolLevel2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(110, 477);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(175, 92);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "BUTTON1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(622, 477);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(175, 92);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "BUTTON2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Location = new System.Drawing.Point(47, 155);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(35, 13);
+            this.txt.TabIndex = 2;
+            this.txt.Text = "label1";
+            this.txt.Click += new System.EventHandler(this.txt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(912, 640);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label txt;
+    }
+}
+
---
+++ EnglishToolLevel2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnglishToolLevel2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txt.Font = new Font("Arial", 24);
+        }
+
+        private void txt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string[] day1 =
+            {
+            };
+
+            string[] ConcatenateArrays(params string[][] arrays)
+            {
+                return arrays.SelectMany(a => a).ToArray();
+            }
+
+            string[] arrtxt = ConcatenateArrays(day1);
+
+            Random randomTxt = new Random();
+            txt.Text = arrtxt[randomTxt.Next(0, arrtxt.Length)];
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            string[] day1 =
+            {
+            };
+
+            string[] ConcatenateArrays(params string[][] arrays)
+            {
+                return arrays.SelectMany(a => a).ToArray();
+            }
+
+            string[] arrtxt = ConcatenateArrays(day1);
+
+            Random randomTxt = new Random();
+            txt.Text = arrtxt[randomTxt.Next(0, arrtxt.Length)];
+        }
+    }
+}
